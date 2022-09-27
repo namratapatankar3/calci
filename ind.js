7e8e5c75828fc80e4137b98a9accf72b174cbf75
namrata <namrataspatankar@gmail.com>
2022-09-27 14:08:54 +0530

    first commit

---
+++ ind.js
@@ -0,0 +1,23 @@
+let dis = document.getElementById('display')
+let buttons = Array.from(document.getElementsByClassName('button'))
+buttons.map((bt) => {
+    bt.addEventListener("click", (e) => {
+        switch (e.target.innerText) {
+            case 'C':
+                display.innerText = ''
+                break;
+            case 'del': if (display.innerText) {
+                display.innerText = display.innerText.slice(0, -1)
+            }
+                break;
+            case '=': try {
+                display.innerText = eval(display.innerText)
+            }
+                catch {
+                    display.innerText = 'Error!'
+                }
+                break;
+            default: display.innerText += e.target.innerText
+        }
+    })
+})
